a52d39d409b2eb59536ba979b7eb57c3264e31d0
Ashly Lau <ashlylau@gmail.com>
2020-02-09 07:10:48 +0000

    formatted image stream to pass to image segmentor and converted back to a stream

--- image_seg/app.py
+++ image_seg/app.py
@@ -4,32 +4,40 @@ import numpy as np
 
 app = Flask(__name__)
 
-segm = Segmentor()
 
 @app.route('/segment', methods=['GET', 'POST'])
 def segment_image():
     # @after_this_request
     # def add_header(response):
     #     response.headers['Access-Control-Allow-Origin'] = '*'
+    segm = Segmentor()
 
     # POST request
     if request.method == 'POST':
         # width = request.form.width
         # height = request.form.height
         data = request.form
-        print(data)
+        # print(data)
         # data = request.get_json()
+        print(type(data.to_dict()['file']))
+        string = data.to_dict()['file']
+        arr = np.fromstring(string, dtype=int, sep=',')
+        print(arr[:10])
+        # image = np.array(list(data.to_dict()['file']), dtype=int)
         # print(data)
-        # image = np.array(data.to_dict(), dtype=int)
-        # print(data)
-        image = data.reshape(300,400,4)
+        image = arr.reshape(300,400,4)
+        rgb = image[:,:,:-1]
         # print(height)
         print("=============================================")
-        seg_map = segm.get_segment_map(image)
-        out_image = segm.get_foreground(image, seg_map)
-        print(image)
-        # print(request.get_json())  # parse as JSON
-        return 200, 'OK'
+        seg_map = segm.get_segment_map(rgb.copy())
+        out_image = get_foreground(rgb, seg_map)
+        print(rgb)
+
+        image[:,:,:-1] = out_image
+
+        out_stream = out_image.flatten()
+
+        return {'file':np.array2string(out_stream, separator=",")[1:-1]}
 
     # GET request
     else:
