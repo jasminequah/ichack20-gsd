aafa342c1196cda2f182f389a26e6229bbdfa8d0
estherwong777 <estherwong19@gmail.com>
2020-02-09 00:14:43 +0000

    Implement video merger

--- README.md
+++ README.md
@@ -1,20 +1,2 @@
-```
-npm install --global browserify watchify
-npm install
-```
-
-Open `public/app.js` with your favourite editor and replace `<insert_your_access_token_here>` on line 3 with your own access token.
-
-Then run:
-```
-watchify public/app.js -o public/bundle.js -v
-```
-
-This will package up all of the dependencies in app.js into a bundle and automatically repeat this whenever app.js changes.
-
-Keep that running and in a separate terminal window run:
-
-```
-node server.js
-```
+Setup python HTTP server by running `python -m http.server`
 
---
+++ script.js
@@ -0,0 +1,146 @@
+// Generate random room name if needed
+// var VideoStreamMerger = require('video-stream-merger');
+
+var merger = new VideoStreamMerger();
+merger.start();
+
+if (!location.hash) {
+  location.hash = Math.floor(Math.random() * 0xFFFFFF).toString(16);
+}
+const roomHash = location.hash.substring(1);
+
+// TODO: Replace with your own channel ID
+const drone = new ScaleDrone('yiS12Ts5RdNhebyM');
+// Room name needs to be prefixed with 'observable-'
+const roomName = 'observable-' + roomHash;
+const configuration = {
+  iceServers: [{
+    urls: 'stun:stun.l.google.com:19302'
+  }]
+};
+let room;
+let pc;
+
+
+function onSuccess() {};
+function onError(error) {
+  console.error(error);
+};
+
+drone.on('open', error => {
+  if (error) {
+    return console.error(error);
+  }
+  room = drone.subscribe(roomName);
+  room.on('open', error => {
+    if (error) {
+      onError(error);
+    }
+  });
+  // We're connected to the room and received an array of 'members'
+  // connected to the room (including us). Signaling server is ready.
+  room.on('members', members => {
+    console.log('MEMBERS', members);
+    // If we are the second user to connect to the room we will be creating the offer
+    const isOfferer = members.length === 2;
+    startWebRTC(isOfferer);
+  });
+});
+
+// Send signaling data via Scaledrone
+function sendMessage(message) {
+  drone.publish({
+    room: roomName,
+    message
+  });
+}
+
+function startWebRTC(isOfferer) {
+  pc = new RTCPeerConnection(configuration);
+
+  // 'onicecandidate' notifies us whenever an ICE agent needs to deliver a
+  // message to the other peer through the signaling server
+  pc.onicecandidate = event => {
+    if (event.candidate) {
+      sendMessage({'candidate': event.candidate});
+    }
+  };
+
+  // If user is offerer let the 'negotiationneeded' event create the offer
+  if (isOfferer) {
+    pc.onnegotiationneeded = () => {
+      pc.createOffer().then(localDescCreated).catch(onError);
+    }
+  }
+
+  // When a remote stream arrives display it in the #remoteVideo element
+  pc.ontrack = event => {
+    const stream = event.streams[0];
+    merger.addStream(stream, {
+      // draw: mergeStreams,
+      x: 100,
+      y: 100,
+      width: merger.width,
+      height: merger.height,
+      mute: false
+    });
+
+    if (!remoteVideo.srcObject || remoteVideo.srcObject.id !== stream.id) {
+      remoteVideo.srcObject = stream;
+    }
+  };
+
+  // const mergeStreams = (ctx, frame, done) => {
+
+  // }
+
+  navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: true,
+  }).then(stream => {
+    // Display your local video in #localVideo element
+    localVideo.srcObject = stream;
+    merger.addStream(stream, {
+      x: 0,
+      y: 0,
+      width: merger.width,
+      height: merger.height,
+      mute: false
+    });
+    console.log("HERE")
+    mergedVideo.srcObject = merger.result;
+    // Add your stream to be sent to the conneting peer
+    stream.getTracks().forEach(track => pc.addTrack(track, stream));
+  }, onError);
+
+  // Listen to signaling data from Scaledrone
+  room.on('data', (message, client) => {
+    // Message was sent by us
+    if (client.id === drone.clientId) {
+      return;
+    }
+
+    if (message.sdp) {
+      // This is called after receiving an offer or answer from another peer
+      pc.setRemoteDescription(new RTCSessionDescription(message.sdp), () => {
+        // When receiving an offer lets answer it
+        if (pc.remoteDescription.type === 'offer') {
+          pc.createAnswer().then(localDescCreated).catch(onError);
+        }
+      }, onError);
+    } else if (message.candidate) {
+      // Add the new ICE candidate to our connections remote description
+      pc.addIceCandidate(
+        new RTCIceCandidate(message.candidate), onSuccess, onError
+      );
+    }
+  });
+}
+
+function localDescCreated(desc) {
+  pc.setLocalDescription(
+    desc,
+    () => sendMessage({'sdp': pc.localDescription}),
+    onError
+  );
+}
